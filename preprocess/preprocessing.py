095fde3e5bb6e5c8bc24ae188f773cc1345910b5
Hsiaoyun <hsiaoyun0@gmail.com>
2020-02-14 00:24:58 +0800

    add ReadFile

--- preprocess/preprocessing.py
+++ preprocess/preprocessing.py
@@ -1,2 +1,25 @@
-print("ini")
-asdf
+import os
+import argparse
+
+# initial
+parser = argparse.ArgumentParser()
+parser.add_argument('--numberoffile', type=int, default=100)
+
+def ReadFile():
+    #Read File Name
+    CNNFileName = os.listdir("/share/corpus/CNN_DailyMail/CNN_DailyMail/origin(DMQA)/cnn/stories")
+
+    # Read File
+    for filename in CNNFileName :
+        filename = '/share/corpus/CNN_DailyMail/CNN_DailyMail/origin(DMQA)/cnn/stories/'+ filename
+        f = open(filename, "r")
+        print(f.read())
+
+
+def __init__(self, needamount):
+    self.needamount = needamount
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    print(args.numberoffile)
