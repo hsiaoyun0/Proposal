9bb9a9c692eafd7219a56ddf6079150201c2852c
Hsiaoyun <hsiaoyun0@gmail.com>
2020-02-14 19:27:22 +0800

    Finish MakeFile

--- preprocess/preprocessing.py
+++ preprocess/preprocessing.py
@@ -5,6 +5,7 @@ import re
 # initial
 parser = argparse.ArgumentParser()
 parser.add_argument('--numberoffile', type=int, default=100)
+parser.add_argument('--numberofans', type=int, default=3)
 
 def ReadFile(numberoffile):
     
@@ -23,8 +24,44 @@ def ReadFile(numberoffile):
 
     return NeedFile
 
+def ProduceAnswer(FileContent, numberofans):
+    
+    DocumentPair=[]
+    flag = FileContent[0].count("@highlight")
+    
+    if flag < numberofans:
+        numberofans = flag
+    print(flag)
+    for Content in FileContent:
+        ans=[]
+        context=[]
+        for Sen in Content[-(flag*2):-(flag-numberofans)]:
+            if Sen != "@highlight":
+                ans.append(Sen)
+        
+        for Sen in Content:
+             if Sen == "@highlight": break
+             context.append(Sen)
+        
+        DocumentPair.append([context,ans])
+     
+    return DocumentPair
+
+def MakeFile(args, DocumentPair):
+    FileName = "CNN_"+str(args.numberofans)+"ans_"+str(args.numberoffile)+"files.txt"
+    with open(FileName,"w") as Writein :
+        for Pair in DocumentPair:
+            Writein.write("Story\n")
+            for Sen in Pair[0] :
+                Writein.write(Sen)
+            Writein.write("\nAnswer\n")
+            for Sen in Pair[1]:
+                Writein.write(Sen)
+                Writein.write(" ")
+            Writein.write("\n\n")
 
 if __name__ == '__main__':
     args = parser.parse_args()
     FileContent = ReadFile(args.numberoffile)
-    print(FileContent)
+    DocumentPair = ProduceAnswer(FileContent, args.numberofans)
+    MakeFile(args, DocumentPair)    
